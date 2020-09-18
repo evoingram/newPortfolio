1f5161b3ed422e40401f405c9446ecffe5e2a0e5
Erica L. Ingram <inquiries@aquoco.co>
2020-09-18 09:04:28 -0700

    resume, about, main sections done

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+function toggleMainSection(sectionName) {
+    var sections = document.getElementsByClassName('submenu');
+    for(let x = 0; x < sections.length; x++){
+        var currentSection = sections[x];
+        if (currentSection.classList.contains('submenu') && currentSection.classList.contains(sectionName)) {
+            currentSection.style.display = 'flex';
+        }
+        else if (!currentSection.classList.contains(sectionName)) {
+            currentSection.style.display = 'none';
+        }
+    }
+  };
+
